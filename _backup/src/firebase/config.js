35dee63ee2ddce0ac080e828a008e9dd1ee93f10
Omotola Bolarin <automotola@users.noreply.github.com>
2017-12-25 23:45:04 +0100

    Fixed backup hiding

--- _backup/src/firebase/config.js
+++
@@ -1,6 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCUll5AyYba1XL8NDYKZ51RGt90KofQo6c',
-  authDomain: 'todo-redux-saga.firebaseapp.com',
-  databaseURL: 'https://todo-redux-saga.firebaseio.com',
-  storageBucket: 'todo-redux-saga.appspot.com'
-};
